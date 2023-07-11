af4dc4d7f06deb71ba2d6fad0af1c284c4b22279
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-07-11 16:18:07 -0300

    feat(lib.rs): all basic manipulations

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,102 @@
-#[must_use]
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use async_openai::types::ChatCompletionRequestMessage;
+use async_openai::types::ChatCompletionResponseMessage;
+
+#[derive(Default)]
+pub struct ChatMemory {
+    /// ID of the model to use.
+    ///
+    /// This is used to select the correct tokenizer for the model.
+    /// See [`CreateChatCompletionRequestArgs.model`].
+    model: String,
+
+    /// The maximum number of tokens to leave for completion.
+    ///
+    /// This is to ensure there's room for the model to generate a response
+    /// given the trimmed context. See [`CreateChatCompletionRequestArgs.
+    /// max_tokens`].
+    max_tokens: u16,
+
+    /// Chat messages in memory.
+    messages: Vec<ChatCompletionRequestMessage>,
+}
+
+impl ChatMemory {
+    #[inline]
+    pub fn model(mut self, model: impl Into<String>) -> Self {
+        self.model = model.into();
+        self
+    }
+
+    #[inline]
+    pub fn max_tokens(mut self, max_tokens: impl Into<u16>) -> Self {
+        self.max_tokens = max_tokens.into();
+        self
+    }
+
+    #[inline]
+    pub fn push(&mut self, message: impl Message) {
+        self.messages.push(message.message());
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn messages(&self) -> &[ChatCompletionRequestMessage] {
+        // TODO: correct
+        self.as_ref()
+    }
+}
+
+impl AsRef<[ChatCompletionRequestMessage]> for ChatMemory {
+    #[inline]
+    fn as_ref(&self) -> &[ChatCompletionRequestMessage] {
+        &self.messages
+    }
+}
+
+impl AsMut<[ChatCompletionRequestMessage]> for ChatMemory {
+    #[inline]
+    fn as_mut(&mut self) -> &mut [ChatCompletionRequestMessage] {
+        &mut self.messages
+    }
+}
+
+impl<M> Extend<M> for ChatMemory
+where
+    M: Message,
+{
+    #[inline]
+    fn extend<T: IntoIterator<Item = M>>(&mut self, messages: T) {
+        self.messages
+            .extend(messages.into_iter().map(Message::message));
+    }
+}
+
+pub trait Message {
+    fn message(self) -> ChatCompletionRequestMessage;
+}
+
+impl Message for ChatCompletionRequestMessage {
+    #[inline]
+    fn message(self) -> ChatCompletionRequestMessage {
+        self
+    }
+}
+
+impl Message for ChatCompletionResponseMessage {
+    #[inline]
+    fn message(self) -> ChatCompletionRequestMessage {
+        ChatCompletionRequestMessage {
+            role: self.role,
+            content: self.content,
+            name: None,
+            function_call: self.function_call,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
