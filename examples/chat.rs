ca533f515fc9913f4122f19c1dab98982b2ed1fa
Felipe S. S. Schneider <37125+schneiderfelipe@users.noreply.github.com>
2023-07-13 19:24:27 -0300

    feat: implement the basics (#1)
    This commit implements chat splitting facilities for OpenAI's chat models. It mainly uses tiktoken-rs and async-openai crates. Chats can be split by both maximum chat completion token counts and chat message counts.

---
+++ examples/chat.rs
@@ -0,0 +1,75 @@
+use std::error::Error;
+
+use async_openai::types::ChatCompletionRequestMessage;
+use async_openai::types::ChatCompletionRequestMessageArgs;
+use async_openai::types::CreateChatCompletionRequestArgs;
+use async_openai::types::Role;
+use async_openai::Client;
+use chat_memory::ChatSplitter;
+use chat_memory::IntoRequestMessage;
+
+const MODEL: &str = "gpt-3.5-turbo";
+const MAX_TOKENS: u16 = 1024;
+const MAX_MESSAGES: usize = 16;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut stored_messages = get_stored_messages()?;
+    stored_messages.push(
+        ChatCompletionRequestMessageArgs::default()
+            .role(Role::User)
+            .content("Where was it played?")
+            .build()?,
+    );
+    assert!(stored_messages.len() > MAX_MESSAGES);
+
+    let (_previous_messages, recent_messages) = ChatSplitter::new(MODEL)
+        .max_tokens(MAX_TOKENS)
+        .max_messages(MAX_MESSAGES)
+        .split(&stored_messages);
+
+    let mut messages = vec![ChatCompletionRequestMessageArgs::default()
+        .role(Role::System)
+        .content("You are a helpful assistant.")
+        .build()?];
+    messages.extend(recent_messages.iter().cloned());
+    assert!(messages.len() <= MAX_MESSAGES + 1);
+
+    let request = CreateChatCompletionRequestArgs::default()
+        .model(MODEL)
+        .max_tokens(MAX_TOKENS)
+        .messages(messages)
+        .build()?;
+
+    let client = Client::new();
+    let response = client.chat().create(request).await?;
+
+    println!("\nResponse:\n");
+    for choice in response.choices {
+        println!(
+            "{}: Role: {}  Content: {:?}",
+            choice.index, choice.message.role, choice.message.content
+        );
+
+        stored_messages.push(choice.message.into_async_openai());
+    }
+
+    Ok(())
+}
+
+fn get_stored_messages() -> Result<Vec<ChatCompletionRequestMessage>, Box<dyn Error>> {
+    let mut messages = Vec::new();
+    for _ in 0..2000 {
+        messages.extend([
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::User)
+                .content("Who won the world series in 2020?")
+                .build()?,
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::Assistant)
+                .content("The Los Angeles Dodgers won the World Series in 2020.")
+                .build()?,
+        ]);
+    }
+    Ok(messages)
+}
---
+++ src/lib.rs
@@ -0,0 +1,290 @@
+use std::cmp::Ordering;
+
+use indxvec::Search;
+use tiktoken_rs::get_chat_completion_max_tokens;
+use tiktoken_rs::model::get_context_size;
+
+/// Chat splitter.
+pub struct ChatSplitter {
+    /// Model to use.
+    ///
+    /// The model is passed to `tiktoken-rs` to select the correct tokenizer.
+    model: String,
+
+    /// The maximum number of tokens to leave for chat completion.
+    ///
+    /// This is the same as in the [official API](https://platform.openai.com/docs/api-reference/chat#completions/create-prompt) and given to `async-openai`.
+    /// The total length of input tokens and generated tokens is limited by the
+    /// model's context length. Splits will have at least that many tokens
+    /// available for chat completion, never less.
+    max_tokens: u16,
+
+    /// The maximum number of messages to have in the chat.
+    ///
+    /// Splits will have at most that many messages, never more.
+    max_messages: usize,
+}
+
+/// Hard limit that seems to be imposed by the `OpenAI` API.
+const MAX_MESSAGES_LIMIT: usize = 2_048;
+
+/// Recommended minimum for maximum chat completion tokens.
+const RECOMMENDED_MIN_MAX_TOKENS: u16 = 256;
+
+impl Default for ChatSplitter {
+    #[inline]
+    fn default() -> Self {
+        Self::new("gpt-3.5-turbo")
+    }
+}
+
+impl ChatSplitter {
+    /// Create a new chat splitter for the given model.
+    ///
+    /// # Panics
+    ///
+    /// If for some reason `tiktoken-rs` gives a context size twice as large as
+    /// what would fit in a `u16`.
+    #[inline]
+    pub fn new(model: impl Into<String>) -> Self {
+        let model = model.into();
+        let max_tokens = u16::try_from(get_context_size(&model) / 2).unwrap();
+
+        let max_messages = MAX_MESSAGES_LIMIT / 2;
+
+        Self {
+            model,
+            max_tokens,
+            max_messages,
+        }
+    }
+
+    /// Set the maximum number of messages for future splits.
+    #[inline]
+    #[must_use]
+    pub fn max_messages(mut self, max_messages: impl Into<usize>) -> Self {
+        self.max_messages = max_messages.into();
+        if self.max_messages > MAX_MESSAGES_LIMIT {
+            log::warn!(
+                "max_messages = {} > {MAX_MESSAGES_LIMIT}",
+                self.max_messages
+            );
+        }
+        self
+    }
+
+    /// Set the maximum number of chat completion tokens for future splits.
+    #[inline]
+    #[must_use]
+    pub fn max_tokens(mut self, max_tokens: impl Into<u16>) -> Self {
+        self.max_tokens = max_tokens.into();
+        if self.max_tokens < RECOMMENDED_MIN_MAX_TOKENS {
+            log::warn!(
+                "max_tokens = {} < {RECOMMENDED_MIN_MAX_TOKENS}",
+                self.max_tokens
+            );
+        }
+        self
+    }
+
+    /// Set the model.
+    ///
+    /// The model is passed to `tiktoken-rs` to select the correct tokenizer.
+    #[inline]
+    #[must_use]
+    pub fn model(mut self, model: impl Into<String>) -> Self {
+        self.model = model.into();
+        self
+    }
+
+    /// Get a split position by only considering `max_messages`.
+    #[inline]
+    fn position_by_max_messages<M>(&self, messages: &[M]) -> usize
+    where
+        M: IntoRequestMessage + Clone,
+    {
+        let upper_limit = self.max_messages.min(MAX_MESSAGES_LIMIT);
+
+        let n = messages.len();
+        let n = if n <= upper_limit { 0 } else { n - upper_limit };
+        debug_assert!(messages[n..].len() <= upper_limit);
+        n
+    }
+
+    /// Get a split position by only considering `max_tokens`.
+    ///
+    /// # Panics
+    ///
+    /// If tokenizer for the specified model is not found or is not a supported
+    /// chat model.
+    #[inline]
+    fn position_by_max_tokens<M>(&self, messages: &[M]) -> usize
+    where
+        M: IntoRequestMessage + Clone,
+    {
+        let max_tokens = self.max_tokens as usize;
+        let lower_limit = max_tokens.min(get_context_size(&self.model));
+
+        let messages: Vec<_> = messages
+            .iter()
+            .cloned()
+            .map(IntoRequestMessage::into_tiktoken_rs)
+            .collect();
+
+        let (n, _range) = (0..=messages.len()).binary_any(|n| {
+            debug_assert!(n < messages.len());
+
+            let tokens = get_chat_completion_max_tokens(&self.model, &messages[n..])
+                .expect("tokenizer should be available");
+
+            let cmp = tokens.cmp(&lower_limit);
+            debug_assert_ne!(cmp, Ordering::Equal);
+            cmp
+        });
+
+        debug_assert!(
+            get_chat_completion_max_tokens(&self.model, &messages[n..])
+                .expect("tokenizer should be available")
+                >= lower_limit
+        );
+        n
+    }
+
+    /// Get a split position by first considering the `max_messages` limit, then
+    /// the `max_tokens` limit.
+    ///
+    /// # Panics
+    ///
+    /// If tokenizer for the specified model is not found or is not a supported
+    /// chat model.
+    #[inline]
+    fn position<M>(&self, messages: &[M]) -> usize
+    where
+        M: IntoRequestMessage + Clone,
+    {
+        let n = self.position_by_max_messages(messages);
+        n + self.position_by_max_tokens(&messages[n..])
+    }
+
+    /// Split the chat into two groups of messages, the old and the most recent
+    /// ones.
+    ///
+    /// The most recent messages are guaranteed to satisfy the given limits, the
+    /// old ones contain all the previous ones.
+    ///
+    /// # Panics
+    ///
+    /// If tokenizer for the specified model is not found or is not a supported
+    /// chat model.
+    #[inline]
+    pub fn split<'a, M>(&self, messages: &'a [M]) -> (&'a [M], &'a [M])
+    where
+        M: IntoRequestMessage + Clone,
+    {
+        messages.split_at(self.position(messages))
+    }
+}
+
+/// Extension traits for converting to different completion request message
+/// types.
+pub trait IntoRequestMessage {
+    /// Convert to `tiktoken-rs` completion request message.
+    fn into_tiktoken_rs(self) -> tiktoken_rs::ChatCompletionRequestMessage;
+
+    /// Convert to `async-openai` completion request message.
+    fn into_async_openai(self) -> async_openai::types::ChatCompletionRequestMessage;
+}
+
+impl IntoRequestMessage for tiktoken_rs::ChatCompletionRequestMessage {
+    #[inline]
+    fn into_tiktoken_rs(self) -> tiktoken_rs::ChatCompletionRequestMessage {
+        self
+    }
+
+    #[inline]
+    fn into_async_openai(self) -> async_openai::types::ChatCompletionRequestMessage {
+        async_openai::types::ChatCompletionRequestMessage {
+            role: match self.role.as_ref() {
+                "user" => async_openai::types::Role::User,
+                "system" => async_openai::types::Role::System,
+                "assistant" => async_openai::types::Role::Assistant,
+                "function" => async_openai::types::Role::Function,
+                role => panic!("unknown role '{role}'"),
+            },
+            content: self.content,
+            function_call: self.function_call.map(|fc| {
+                async_openai::types::FunctionCall {
+                    name: fc.name,
+                    arguments: fc.arguments,
+                }
+            }),
+
+            name: self.name,
+        }
+    }
+}
+
+impl IntoRequestMessage for async_openai::types::ChatCompletionRequestMessage {
+    #[inline]
+    fn into_tiktoken_rs(self) -> tiktoken_rs::ChatCompletionRequestMessage {
+        tiktoken_rs::ChatCompletionRequestMessage {
+            role: self.role.to_string(),
+            content: self.content,
+            function_call: self.function_call.map(|fc| {
+                tiktoken_rs::FunctionCall {
+                    name: fc.name,
+                    arguments: fc.arguments,
+                }
+            }),
+
+            name: self.name,
+        }
+    }
+
+    #[inline]
+    fn into_async_openai(self) -> async_openai::types::ChatCompletionRequestMessage {
+        self
+    }
+}
+
+impl IntoRequestMessage for async_openai::types::ChatCompletionResponseMessage {
+    #[inline]
+    fn into_tiktoken_rs(self) -> tiktoken_rs::ChatCompletionRequestMessage {
+        tiktoken_rs::ChatCompletionRequestMessage {
+            role: self.role.to_string(),
+            content: self.content,
+            function_call: self.function_call.map(|fc| {
+                tiktoken_rs::FunctionCall {
+                    name: fc.name,
+                    arguments: fc.arguments,
+                }
+            }),
+
+            name: None,
+        }
+    }
+
+    #[inline]
+    fn into_async_openai(self) -> async_openai::types::ChatCompletionRequestMessage {
+        async_openai::types::ChatCompletionRequestMessage {
+            role: self.role,
+            content: self.content,
+            function_call: self.function_call,
+
+            name: None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let messages: Vec<async_openai::types::ChatCompletionRequestMessage> = Vec::new();
+
+        assert_eq!(ChatSplitter::default().split(&messages).0, &[]);
+        assert_eq!(ChatSplitter::default().split(&messages).1, &[]);
+    }
+}
